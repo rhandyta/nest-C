31935fcfd65b3aa819fc7778c3f8f99ba8d30358
rhandyta <rhandytabriantama@gmail.com>
2023-10-19 21:26:26 +0700

    user register and show profile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: CreateUserDto) {
+    const emailUnique = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    })
+
+    if(emailUnique) {
+      throw new ConflictException("Data sudah dipakai")
+    }
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...dto,
+        password: await hash(dto.password, 10),
+      },
+    });
+
+    const { password, ...user } = newUser;
+    return user;
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +38,15 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if(!user) throw new NotFoundException('Data tidak ditemukan')
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
